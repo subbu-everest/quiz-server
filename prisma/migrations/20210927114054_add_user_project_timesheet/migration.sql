27e5c6f9be6b704d5c33c64f10ce60276760f9de
Subbu <svenkat683@gmail.com>
2021-09-27 17:11:50 +0530

    feat(migrations): add user, project and timesheet

---
+++ prisma/migrations/20210927114054_add_user_project_timesheet/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "timesheet" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "user_id" UUID NOT NULL,
+    "hours_worked" INTEGER NOT NULL,
+    "project_id" UUID NOT NULL,
+
+    CONSTRAINT "timesheet_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "user" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "projects" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "timesheet" ADD CONSTRAINT "timesheet_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "timesheet" ADD CONSTRAINT "timesheet_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
